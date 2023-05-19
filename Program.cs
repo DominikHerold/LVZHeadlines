953e0310403cdbbc0c2af05686257e6d0258165c
Dominik Herold <d.herold@cid.com>
2023-05-19 20:16:41 +0200

    stub

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) => { })
+    .Build();
+
+static async Task StartAnalysisAsync(IHost host)
+{
+    await Task.CompletedTask;
+
+    Console.WriteLine("LVZ Headlines");
+
+    Environment.Exit(0);
+}
+
+await StartAnalysisAsync(host);
+await host.RunAsync();
