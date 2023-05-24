4f23a2abd4f218fa3bb83048eace86de13b5df85
Dominik Herold <d.herold@cid.com>
2023-05-24 11:43:45 +0200

    send changes via PushOver

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using CodeHollow.FeedReader;
 using Flurl.Http;
 using Microsoft.Extensions.Hosting;
@@ -10,7 +11,7 @@ using IHost host = Host.CreateDefaultBuilder(args)
 static string CreateMD5(string input)
 {
     using var md5 = MD5.Create();
-    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+    byte[] inputBytes = Encoding.ASCII.GetBytes(input);
     byte[] hashBytes = md5.ComputeHash(inputBytes);
 
     return Convert.ToHexString(hashBytes);
@@ -28,7 +29,10 @@ static async Task StartAnalysisAsync(IHost host)
 
     Console.WriteLine("Feed Title: " + feed.Title);
     Console.WriteLine("Feed Description: " + feed.Description);
-    // ...
+
+    var pushOverToken = Environment.GetEnvironmentVariable("PUSH_OVER_TOKEN");
+    var pushOverUser = Environment.GetEnvironmentVariable("PUSH_OVER_USER");
+
     foreach (var item in feed.Items)
     {
         Console.WriteLine(item.Id);
@@ -55,10 +59,26 @@ static async Task StartAnalysisAsync(IHost host)
                 tokenSource.Token);
 
             var titleFullPath = Path.Combine("./Title", CreateMD5(id));
+            var oldTitle = string.Empty;
+            if (File.Exists(titleFullPath))
+            {
+                oldTitle = await File.ReadAllTextAsync(titleFullPath, tokenSource.Token);
+            }
+
             await File.WriteAllTextAsync(
                 titleFullPath,
                 title,
                 tokenSource.Token);
+
+            if (!string.IsNullOrEmpty(oldTitle) && oldTitle != title)
+            {
+                var client = new HttpClient();
+                var toSend = $"token={pushOverToken}&user={pushOverUser}&message={oldTitle} ➡️ {title}";
+
+                client.PostAsync("https://api.pushover.net/1/messages.json",
+                        new StringContent(toSend, Encoding.UTF8, "application/x-www-form-urlencoded")).GetAwaiter()
+                    .GetResult();
+            }
         }
     }
 
